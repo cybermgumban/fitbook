feba0f9f11c2a672afd5dde4fa8b6a84d46b871a
Marlon Gumban <gumban.marlon@yahoo.com>
2019-06-12 20:18:02 +0800

    Replaced postgres with mongoose and add dummy date for graphql

--- server/App.js
+++ server/App.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-//const Schema = require('./Schema/Schema');
+const Schema = require('./Schema/Schema');
+const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
@@ -9,32 +10,18 @@ const { Client } = require('pg');
 //allow coss-origin requests
 app.use(cors());
 
-//connect to Postgresql server
-const connectionString = 'postgres://emhkraqcuohsus:e64cb126ad6294da3272ab2ec30e48afd26c5d248e09e2bdcb73b18a4366db29@ec2-75-101-128-10.compute-1.amazonaws.com:5432/dda75h13i7ktd9';
-
-const client = new Client({
-  connectionString: connectionString,
-})
-
-client.connect((err) => {
-    if (err) {
-      console.error('connection error', err.stack)
-    } else {
-      console.log('connected to database')
-    }
-})
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log("connected to database")
-  client.end()
+//connect to mLab database
+mongoose.connect('mongodb+srv://marlon:test123@cluster0-a53tv.mongodb.net/test?retryWrites=true')
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
 })
 
 
-//connect to graphQL server
-// app.use('/graphql', graphqlHTTP({
-//     schema: Schema,
-//     graphiql: true
-// }));
+// connect to graphQL server
+app.use('/graphql', graphqlHTTP({
+    schema: Schema,
+    graphiql: true
+}));
 
 app.listen(4000, () => {
     console.log('now listening on request on port 4000');
